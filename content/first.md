3af264ecf613f2c4319ed344c40449a7e41cfa64
nacho <nacho@pinarlabs.com>
2024-11-12 14:00:06 -0300

    initial commit

---
+++ content/first.md
@@ -0,0 +1,5 @@
+Title: My First Review
+Date: 2024-11-12 10:20
+Category: Review
+
+Following is a review of my favorite mechanical keyboard.
---
+++ pelicanconf.py
@@ -0,0 +1,42 @@
+AUTHOR = 'nacho esmite'
+SITENAME = 'pandocreek'
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = 'America/Montevideo'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+MARKDOWN = {
+    'extension_configs': {
+        'markdown.extensions.codehilite': {'css_class': 'highlight'},
+        'markdown.extensions.extra': {},
+        'markdown.extensions.meta': {},
+    },
+    'output_format': 'html5',
+}
+# Blogroll
+LINKS = (
+    ("Pelican", "https://getpelican.com/"),
+    ("Python.org", "https://www.python.org/"),
+    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+    ("You can modify those links in your config file", "#"),
+)
+
+# Social widget
+SOCIAL = (
+    ("You can add links in your config file", "#"),
+    ("Another social link", "#"),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
